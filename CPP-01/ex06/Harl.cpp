26efa6ae32a16786b307f38263f0dfac3adc3217
Viti <vruizgonz@gmail.com>
2024-07-26 18:09:58 +0200

    ex06 done

--- CPP-01/ex06/Harl.cpp
+++ CPP-01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: VR <VR@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 12:15:19 by VR                #+#    #+#             */
-/*   Updated: 2024/07/26 17:27:36 by VR               ###   ########.fr       */
+/*   Updated: 2024/07/26 18:08:19 by VR               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,25 +26,39 @@ Harl::~Harl()
 
 void Harl::debug()
 {
+	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+	std::cout << std::endl;
+	info();
 	return ;
 }
 
 void Harl::info()
 {
+	std::cout << std::endl;
+	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << std::endl;
+	warning();
 	return ;
 }
 
 void Harl::warning()
 {
+	std::cout << std::endl;
+	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << std::endl;
+	error();
 	return ;
 }
 
 void Harl::error()
 {
+	std::cout << std::endl;
+	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << std::endl;
 	return ;
 }
 
@@ -72,7 +86,7 @@ void Harl::complain(std::string level)
 			this->error();
 			break;
 		default:
-			std::cout << "Dont like this option." << std::endl;
+			std::cout << "Probably complaining about insignificant problems." << std::endl;
 			break;
 	}
 }
