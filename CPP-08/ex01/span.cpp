2a94815c974f7ee40f8cad4336b2f76607c51476
Viti <vruizgonz@gmail.com>
2025-07-03 13:22:17 +0200

    avance

--- CPP-08/ex01/span.cpp
+++ CPP-08/ex01/span.cpp
@@ -6,7 +6,51 @@
 /*   By: VR <VR@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 13:36:05 by VR                #+#    #+#             */
-/*   Updated: 2025/02/03 13:36:15 by VR               ###   ########.fr       */
+/*   Updated: 2025/07/01 15:54:03 by VR               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "span.hpp"
+
+Span::Span() 
+{
+	std::cout << "Default Span constructor called." << std::endl;
+	return ;
+}
+
+Span::Span(unsigned int num)
+{
+	maxSize = num;
+	return ;
+}
+
+Span::~Span()
+{
+	std::cout << "Span Destructor called." << std::endl;
+}
+
+Span::Span(const Span& other)
+{
+	*this = other;
+	return ;
+}
+
+Span& Span::operator=(const Span& other)
+{
+	if (this != &other)
+	{
+		this->container = other.container;
+		this->maxSize = other.maxSize;
+	}
+	return *this;
+}
+
+const char* Span::spanFullException::what() const throw()
+{
+	return "cannot add more items, span is full";
+}
+
+const char* Span::notEnoughNumbersException::what() const throw()
+{
+	return "span does not have enough numbers";
+}
--- CPP-08/ex01/span.hpp
+++ CPP-08/ex01/span.hpp
@@ -6,7 +6,7 @@
 /*   By: VR <VR@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 13:36:11 by VR                #+#    #+#             */
-/*   Updated: 2025/02/03 16:40:19 by VR               ###   ########.fr       */
+/*   Updated: 2025/06/30 14:02:55 by VR               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,39 @@
 
 # include <iostream>
 # include <string>
+# include <vector>
  
+class Span 
+{
+	private:
+		std::vector<int> container;
+		unsigned int maxSize;
+		class spanFullException : public std::exception {
+			virtual const char* what() const throw();
+		};
+		class notEnoughNumbersException : public std::exception {
+			virtual const char* what() const throw();
+		};
+
+	public:
+		Span();
+		Span(unsigned int);
+		Span(const Span& other);
+		Span& operator=(const Span& other);
+		~Span();
+
+		int shortestSpan( void ) const;
+		void addNumber( int nbr );
+		int longestSpan( void ) const;	
+		template <typename T>
+		void addManyNumbers(T it_begin, T it_end)
+		{
+			while (it_begin < it_end)
+			{
+				this->addNumber( *it_begin );
+				it_begin++;
+			}
+		}
+} ;
 
 # endif
